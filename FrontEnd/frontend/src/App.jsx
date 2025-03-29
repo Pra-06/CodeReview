eb48706a4d058ac21233c47e555eeb5016044ad1
Pra-06 <150579100+Pra-06@users.noreply.github.com>
2025-03-29 13:41:04 +0530

    completed

--- FrontEnd/frontend/src/App.jsx
+++ FrontEnd/frontend/src/App.jsx
@@ -2,7 +2,12 @@ import { useState ,useEffect} from 'react'
 import "prismjs/themes/prism-tomorrow.css"
 import Editor  from "react-simple-code-editor"
 import prism from "prismjs"
+import axios from "axios"
+import Markdown from 'react-markdown'
+import rehypeHighlight from 'rehype-highlight'
+import "highlight.js/styles/github-dark.css"
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,9 +16,22 @@ function App() {
   }`
   )
 
+  const [review,setReview]=useState(``)
+
+
   useEffect(()=>{
     prism.highlightAll()
   })
+
+  
+  async function reviewCode(){
+   
+   
+    const response = await axios.post('http://localhost:3000/ai/get-review',{code})
+    setReview(response.data);
+
+  }
+
   return (
     <>
      <main>
@@ -40,9 +58,18 @@ function App() {
             />
            
         </div>
-        <div className="review">Review</div>
+        <div 
+        onClick={reviewCode}
+        className="review">Review</div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+      <Markdown 
+      rehypePlugins = {[rehypeHighlight]}
+      >
+
+        
+        {review}</Markdown>
+      </div>
      </main>
     </>
   )
