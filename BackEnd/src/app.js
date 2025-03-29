87d77f47697d432f2b3710a0800df461d0033363
Pra-06 <150579100+Pra-06@users.noreply.github.com>
2025-03-29 13:41:41 +0530

    completed

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const aiRoutes = require("./routes/ai.routes")
+const cors = require('cors')
 
 const app = express();
+
+app.use(cors())
+
+
 app.use(express.json())
 
 app.get('/', (req,res)=>{
